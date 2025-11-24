1fe4ffd3500fbd31d5f203bc74939b59ecacefc7
Siddhant2953 <siddhantsharmacs23@pgdav.du.ac.in>
2025-11-24 23:17:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
-/* React code for DU Hostel Management System (omitted for brevity in this step) */
+/* React code for DU Hostel Mimport React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>DU Hostel Management System</h1>
+    </div>
+  );
+}
+
+export default App;
+anagement System (omitted for brevity in this step) */
